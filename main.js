1eedab0780252a10fe47a60e237f64ce6fd98a8f
SouQuemSou <rejectingmediocrity@gmail.com>
2023-01-31 18:09:27 -0300

    up

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-document.querySeletor('tecla_pom')
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+
+const ListaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0
+
+while (contador < ListaDeTeclas.length) {
+    ListaDeTeclas[contador].onclick = tocaSomPom
+    contador = contador + 1 
+}
